5cafdd343e40660d82670aee64dc6857c27bc8a5
Alisson Silva <alissonsilva.py@gmail.com>
2021-01-19 18:41:10 -0300

    :sparkles: feat: Adiciona o registro

---
+++ src/app/models/User.js
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'user',
+      }
+    );
+
+    return this;
+  }
+}
+
+export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
 
+// Controllers
+import UserController from './app/controllers/UserController';
+
+// Requests
+import user from './app/requests/user';
+
 // Router
 const routes = Router();
+
+routes.post('/user/store', user, UserController.store);
 
 export default routes;
